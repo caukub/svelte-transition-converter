b83df2735cd7869194ac2fb429bc34eb11717be5
quick-898 <neznitructible@gmail.com>
2023-11-12 14:49:27 +0100

    remove filehash from file name

--- svelte_transition_converter.js
+++ svelte_transition_converter.js
@@ -533,7 +533,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('svelte_transition_converter-7a8cda78f5645c04_bg.wasm', import.meta.url);
+        input = new URL('svelte_transition_converter_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
